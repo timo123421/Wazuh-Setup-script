ddd8abc9095564443b657c7f0cec71e002122a02
timo123421 <39274235+timo123421@users.noreply.github.com>
2022-05-30 23:37:30 +0200

    create Setup.sh
    This the first version of my script. if you have any ideas to improve it feel free to push a update.

---
+++ setup.sh
@@ -0,0 +1,83 @@
+#adding to Repo
+apt install gnupg apt-transport-https
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+apt-get update
+
+wait 10
+
+
+#install Wazuh Manager
+apt -y install wazuh-manager
+systemctl daemon-reload
+systemctl enable wazuh-manager
+systemctl start wazuh-manager
+wait 10
+
+
+#install elasticsearch
+apt install opendistroforelasticsearch
+curl -so /etc/elasticsearch/elasticsearch.yml https://packages.wazuh.com/resources/4.2/open-distro/elasticsearch/7.x/elasticsearch_all_in_one.yml
+curl -so /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/roles.yml https://packages.wazuh.com/resources/4.2/open-distro/elasticsearch/roles/roles.yml
+curl -so /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/roles_mapping.yml https://packages.wazuh.com/resources/4.2/open-distro/elasticsearch/roles/roles_mapping.yml
+curl -so /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/internal_users.yml https://packages.wazuh.com/resources/4.2/open-distro/elasticsearch/roles/internal_users.yml
+
+wait 10
+
+#install certificate
+rm /etc/elasticsearch/esnode-key.pem /etc/elasticsearch/esnode.pem /etc/elasticsearch/kirk-key.pem /etc/elasticsearch/kirk.pem /etc/elasticsearch/root-ca.pem -f
+curl -so ~/wazuh-cert-tool.sh https://packages.wazuh.com/resources/4.2/open-distro/tools/certificate-utility/wazuh-cert-tool.sh
+curl -so ~/instances.yml https://packages.wazuh.com/resources/4.2/open-distro/tools/certificate-utility/instances_aio.yml
+bash ~/wazuh-cert-tool.sh
+mkdir /etc/elasticsearch/certs/
+mv ~/certs/elasticsearch* /etc/elasticsearch/certs/
+mv ~/certs/admin* /etc/elasticsearch/certs/
+cp ~/certs/root-ca* /etc/elasticsearch/certs/
+systemctl daemon-reload
+systemctl enable elasticsearch
+systemctl start elasticsearch
+
+export JAVA_HOME=/usr/share/elasticsearch/jdk/ && /usr/share/elasticsearch/plugins/opendistro_security/tools/securityadmin.sh -cd /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/ -nhnv -cacert /etc/elasticsearch/certs/root-ca.pem -cert /etc/elasticsearch/certs/admin.pem -key /etc/elasticsearch/certs/admin-key.pem
+curl -XGET https://localhost:9200 -u admin:admin -k
+
+wait 10
+
+#install filebeat
+apt install filebeat
+curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.3/tpl/wazuh/filebeat/filebeat.yml
+curl -so /etc/filebeat/wazuh-template.json
+https://raw.githubusercontent.com/wazuh/wazuh/4.3/extensions/elasticsearch/7.x/wazuh-template.json
+chmod go+r /etc/filebeat/wazuh-template.json
+curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.1.tar.gz | tar -xvz -C /usr/share/filebeat/module
+mkdir /etc/filebeat/certs
+cp ~/certs/root-ca.pem /etc/filebeat/certs/
+mv ~/certs/filebeat* /etc/filebeat/certs/
+systemctl daemon-reload
+systemctl enable filebeat
+systemctl start filebeat
+filebeat test output
+
+wait 10
+
+#install kibana
+apt install opendistroforelasticsearch-kibana
+curl -so /etc/kibana/kibana.yml https://packages.wazuh.com/resources/4.2/open-distro/kibana/7.x/kibana_all_in_one.yml
+mkdir /usr/share/kibana/data
+chown -R kibana:kibana /usr/share/kibana/data
+cd /usr/share/kibana
+sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.2.4_7.10.2-1.zip
+
+mkdir /etc/kibana/certs
+cp ~/certs/root-ca.pem /etc/kibana/certs/
+mv ~/certs/kibana* /etc/kibana/certs/
+chown kibana:kibana /etc/kibana/certs/*
+
+setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node
+systemctl daemon-reload
+systemctl enable kibana
+systemctl start kibana
+
+read -p "your IP " yourip
+echo "URL:https://$yourip"
+echo "Username: admin"
+echo "Password: admin"
